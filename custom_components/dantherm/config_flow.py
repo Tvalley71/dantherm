a63c3af79a5bd97b2971a6b54166723f98406d1c
Tvalley71 <thomas@skovlydahl.dk>
2025-06-24 12:43:40 +0200

    Merged config and options flow

--- custom_components/dantherm/config_flow.py
+++ custom_components/dantherm/config_flow.py
@@ -1,20 +1,24 @@
 """Config Flow implamentation."""
 
 import ipaddress
-import logging
 import re
 
 import voluptuous as vol
 
-from config.custom_components.dantherm.options_flow import DanthermOptionsFlowHandler
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant, callback
+import homeassistant.helpers.entity_registry as er
 
 from .const import DEFAULT_NAME, DEFAULT_PORT, DEFAULT_SCAN_INTERVAL, DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
+from .device_map import (
+    ADAPTIVE_TRIGGERS,
+    ATTR_BOOST_MODE_TRIGGER,
+    ATTR_ECO_MODE_TRIGGER,
+    ATTR_HOME_MODE_TRIGGER,
+    ATTR_TURN_OFF_ALARM_NOTIFICATION,
+    ATTR_TURN_OFF_TEMPERATURE_UNKNOWN,
+)
 
 DATA_SCHEMA = vol.Schema(
     {
@@ -85,3 +89,68 @@ class DanthermConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ):
         """Create the options flow."""
         return DanthermOptionsFlowHandler()
+
+
+class DanthermOptionsFlowHandler(config_entries.OptionsFlow):
+    """Options flow handler."""
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        errors = {}
+
+        STEP_INIT_SCHEMA = vol.Schema(
+            {
+                vol.Optional(ATTR_BOOST_MODE_TRIGGER): str,
+                vol.Optional(ATTR_ECO_MODE_TRIGGER): str,
+                vol.Optional(ATTR_HOME_MODE_TRIGGER): str,
+                vol.Optional(
+                    ATTR_TURN_OFF_TEMPERATURE_UNKNOWN,
+                    default=self.config_entry.options.get(
+                        ATTR_TURN_OFF_TEMPERATURE_UNKNOWN, False
+                    ),
+                ): bool,
+                vol.Optional(
+                    ATTR_TURN_OFF_ALARM_NOTIFICATION,
+                    default=self.config_entry.options.get(
+                        ATTR_TURN_OFF_ALARM_NOTIFICATION, False
+                    ),
+                ): bool,
+            }
+        )
+
+        if user_input is not None:
+            # Validate the user-inputted entities
+            entity_registry = er.async_get(self.hass)
+            for entity_key in ADAPTIVE_TRIGGERS:
+                entity_id = user_input.get(entity_key)
+                if entity_id and entity_id not in [
+                    entity.entity_id for entity in entity_registry.entities.values()
+                ]:
+                    errors[entity_key] = "invalid_entity"
+
+            if not errors:
+                # current_opts = self.config_entry.options or {}
+                # if user_input != current_opts:
+                # Persist the new options
+                self.hass.config_entries.async_update_entry(
+                    self.config_entry, options=user_input
+                )
+
+                # Schedule integration reload on next data update
+                hass_data = self.hass.data[DOMAIN].get(self.config_entry.entry_id)
+                if hass_data and "coordinator" in hass_data:
+                    hass_data["coordinator"].schedule_reload()
+
+                return self.async_create_entry(title="", data=user_input)
+
+            return self.async_show_form(
+                step_id="init", data_schema=STEP_INIT_SCHEMA, errors=errors
+            )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=self.add_suggested_values_to_schema(
+                STEP_INIT_SCHEMA,
+                self.config_entry.options,
+            ),
+        )
